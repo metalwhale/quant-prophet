7a296f48bc2faddcc79ca66406404ba6d544bc34
Metal Whale <metalwhale.dev@gmail.com>
2024-06-04 13:26:37 +0000

    Use candle to store price for asset.

---
+++ trainer/trainer/asset/base.py
@@ -0,0 +1,140 @@
+import datetime
+import random
+from abc import ABC, abstractmethod
+from typing import Dict, List, Optional
+
+
+class DailyCandle:
+    _date: datetime.date
+    _high: float
+    _low: float
+    _close: float
+
+    def __init__(
+        self,
+        date: datetime.date,
+        high: float, low: float, close: float,
+    ) -> "DailyCandle":
+        self._date = date
+        self._high = high
+        self._low = low
+        self._close = close
+
+    @property
+    def date(self) -> datetime.date:
+        return self._date
+
+    @property
+    def high(self) -> float:
+        return self._high
+
+    @property
+    def low(self) -> float:
+        return self._low
+
+    @property
+    def close(self) -> float:
+        return self._close
+
+
+class DailyPrice:
+    _date: datetime.date
+    _actual_price: float  # Close price or a random value between low price and high price
+    _price_delta: float  # Change in price expressed as a ratio compared to the previous day
+
+    def __init__(self, date: datetime.date, actual_price: float, price_delta: float) -> "DailyPrice":
+        self._date = date
+        self._actual_price = actual_price
+        self._price_delta = price_delta
+
+    @property
+    def date(self) -> datetime.date:
+        return self._date
+
+    @property
+    def actual_price(self) -> float:
+        return self._actual_price
+
+    @property
+    def price_delta(self) -> float:
+        return self._price_delta
+
+
+class DailyAsset(ABC):
+    __candles: List[DailyCandle]
+    __candle_indices: Dict[str, int]
+
+    __DATE_FORMAT = "%Y-%m-%d"
+
+    # Remember to call this method in the inheritance class to fetch candles
+    def initialize(self):
+        self.__candles = self._fetch_candles()
+        self.__candles.sort(key=lambda candle: candle.date)  # Sort the list just in case
+        self.__candle_indices = {
+            c.date.strftime(self.__DATE_FORMAT): i
+            for i, c in enumerate(self.__candles)
+        }
+
+    # Find the widest date range that matches the following conditions:
+    # - Chosen from the dates of `self.__candles`, skip the first `historical_days_num` dates and maintain the order
+    # - All dates must be equal to or greater than `min_date`
+    # - All dates must be equal to or smaller than `max_date`
+    def find_matched_tradable_date_range(
+        self,
+        historical_days_num: int,
+        min_date: Optional[datetime.date] = None, max_date: Optional[datetime.date] = None,
+    ) -> List[datetime.date]:
+        if (
+            (min_date is not None and max_date is not None and min_date > max_date)
+            # We need at least `historical_days_num` plus 1 because price delta requires one previous day to calculate
+            or len(self.__candles) < historical_days_num + 1
+        ):
+            return []
+        date_range: List[datetime.date] = []
+        for candle in self.__candles[historical_days_num:]:
+            date = candle.date
+            if min_date is not None and date < min_date:
+                continue
+            if max_date is not None and date > max_date:
+                break
+            date_range.append(date)
+        return date_range
+
+    # Returns prices within a specified date range, defined by an end date and the number of days to retrieve.
+    # The actual price used for calculating price delta is usually the close price, except for end date,
+    # where it should be randomly chosen within the range of low price to high price.
+    def retrieve_historical_prices(self, end_date: datetime.date, days_num: int) -> List[DailyPrice]:
+        end_date_index = self.__candle_indices.get(end_date.strftime(self.__DATE_FORMAT))
+        if end_date_index is None or end_date_index < days_num - 1:
+            return []
+        prices: List[DailyPrice] = []
+        # Historical prices for the days before `end_date`
+        for i in range(end_date_index - (days_num - 1), end_date_index):
+            prices.append(DailyPrice(
+                self.__candles[i].date,
+                self.__candles[i].close,
+                self.__candles[i].close / self.__candles[i - 1].close - 1,
+            ))
+        # Price for `end_date`
+        end_date_price = 0
+        if end_date == datetime.datetime.now().date():
+            end_date_price = self._fetch_spot_price()
+        else:
+            end_date_price = self.__candles[end_date_index].low \
+                + random.random() * (self.__candles[end_date_index].high - self.__candles[end_date_index].low)
+        prices.append(DailyPrice(
+            self.__candles[end_date_index].date,
+            end_date_price,
+            (end_date_price / self.__candles[end_date_index - 1].close - 1)
+        ))
+        return prices
+
+    # Returns list of candles in ascending order of date
+    @abstractmethod
+    def _fetch_candles(self) -> List[DailyCandle]:
+        pass
+
+    # Returns the price at the current moment
+    @abstractmethod
+    def _fetch_spot_price(self) -> float:
+        pass
--- trainer/trainer/trading_platform.py
+++ trainer/trainer/trading_platform.py
@@ -8,7 +8,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import markers
 
-from .asset import DailyAsset, Price
+from .asset.base import DailyAsset, DailyPrice
 
 
 class PositionType(IntEnum):
@@ -17,26 +17,35 @@ class PositionType(IntEnum):
 
 
 class Position:
-    _time: datetime.datetime
-    _amount: float
+    _date: datetime.date
     _position_type: PositionType
+    _entry_price: float
+    _amount: float
 
-    def __init__(self, time: datetime.datetime, amount: float, position_type: PositionType) -> "Position":
-        self._time = time
-        self._amount = amount
+    def __init__(
+        self,
+        date: datetime.date, position_type: PositionType, entry_price: float, amount: float,
+    ) -> "Position":
+        self._date = date
         self._position_type = position_type
+        self._entry_price = entry_price
+        self._amount = amount
 
     @property
-    def time(self) -> datetime.datetime:
-        return self._time
-
-    @property
-    def amount(self) -> float:
-        return self._amount
+    def date(self) -> datetime.date:
+        return self._date
 
     @property
     def position_type(self) -> PositionType:
         return self._position_type
+
+    @property
+    def entry_price(self) -> float:
+        return self._entry_price
+
+    @property
+    def amount(self) -> float:
+        return self._amount
 
 
 # Doc:
@@ -54,8 +63,8 @@ class TradingPlatform(gym.Env):
     # - A "date" refers to a "tradable day of the date type", i.e., a day on which we can open an position.
     #   We use "date" as the end date of a date range when retrieving historical data.
     #   Note that, unless otherwise explained, we consider a "date" to have this meaning.
-    # - An example of a date that is not a "date" according to our definition is the "first date" of an asset,
-    #   and all dates before the "initial date" of an episode (see the `reset` method for details),
+    # - An example of a date that is not a "date" according to our definition is the "published date" of an asset,
+    #   and all dates before the "first date" of an episode (see the `reset` method for details),
     #   because they are not used as the end date when retrieving historical data or for opening positions,
     #   but rather as serial data used for training the model.
     #
@@ -63,21 +72,26 @@ class TradingPlatform(gym.Env):
     #   (Note: `~~~~~~~~*` depicts length of `_historical_days_num`,
     #    with `*` being the date used as the end date when retrieving the historical data)
     #
-    #   |<---Unused--->|<--------------------Training data---------------------->|<-------------Validation data--------------->|
-    #   |              |                                                         |                                             |
-    #   o======================o=================================================o=============================================o
-    #   |              |       |                                  |              |
-    #   |              |       |                                  |              Last training date (`_last_training_date`)
-    #   |              |       |                          ~~~~~~~~*
-    #   |              |       |                                  The "date" will continuously grow until the last training date
-    #   |              ~~~~~~~~*
-    #   |                      Episode's "initial date" (`_initial_date`), randomly chosen within range
-    #   |                      from the asset's "first date" plus number of historical data, to the day before last training date
+    #   Asset's "published date"
     #   |
-    #   |<-------------------->| All dates before "initial date" are non-tradable,
-    #   |                      | and have at least the length of `_historical_days_num`
+    #   |       |<---------------------------------------- Asset's tradable date range ---------------------------------------->|
+    #   |       |<------------------------ Training data ------------------------>|<------------- Validation data ------------->|
+    #   |       |                                                                 |                                             |
+    #   ~~~~~~~~|=================================================================|=============================================| Now
+    #   |       |                                                                 |
+    #   |                                                                         Last training date (`_last_training_date`)
     #   |
-    #   Asset's "first date" (`asset_first_date`)
+    #   |<----->| The first `historical_days_num` dates are non-tradable and will be skipped
+    #
+    #                          |<------------- Episode's date range ------------->|
+    #                          |==================================================|
+    #                          |                                                  |
+    #                          |                          ~~~~~~~~*     ---->     |
+    #                          |                                  |
+    #                          |                                  The "date" will continuously grow until the last training date
+    #                  ~~~~~~~~*
+    #                          |
+    #                          Episode's "first date", randomly chosen within training data's date range
 
     # Hyperparameters
     _asset_pool: List[DailyAsset]
@@ -98,9 +112,9 @@ class TradingPlatform(gym.Env):
 
     # State components
     _asset_index: int  # Changed only if we reset
-    _initial_date: datetime.date  # The random initial date of each episode
-    _date: datetime.date  # Grows in the same episode, resets to initial date for a new episode
-    _prices: List[Price]  # Updated whenever the date changes
+    _date_range: List[datetime.date]  # The random date range of each episode
+    _date_index: int  # Grows in the same episode, resets to 0 for a new episode
+    _prices: List[DailyPrice]  # Updated whenever the date changes
     _positions: List[Position]  # Keeps adding positions in the same episode, clears them all for a new episode
     _balance: float  # Resets to initial balance after each episode
 
@@ -153,35 +167,36 @@ class TradingPlatform(gym.Env):
     def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[Dict[str, Any], dict[str, Any]]:
         super().reset(seed=seed, options=options)
         self._asset_index = self.np_random.integers(0, high=len(self._asset_pool))
-        asset_first_date = self._asset.get_first_date()
-        # Randomly choose a date within range from the asset's first date plus number of historical data
-        # to the day before last training date, and consider it as end date for retrieving historical prices.
-        self._initial_date = asset_first_date \
-            + datetime.timedelta(days=float(self.np_random.integers(
-                self._historical_days_num - 1,
-                high=(self._last_training_date - asset_first_date).days,  # Exclusive
-            )))
-        self._date = self._initial_date
+        # Randomly choose a date within asset's tradable date range
+        asset_tradable_date_range = self._asset.find_matched_tradable_date_range(
+            self._historical_days_num,
+            max_date=self._last_training_date,
+        )
+        # Date range need to have at least two dates, one for the reset and one for a single step
+        self._date_range = asset_tradable_date_range[self.np_random.integers(len(asset_tradable_date_range) - 1):]
+        self._date_index = 0
         self._retrieve_prices()
         self._positions = [Position(
-            self._prices[-1].time, self._position_amount,
-            self.np_random.choice([PositionType.LONG, PositionType.SHORT]),
+            self._prices[-1].date, self.np_random.choice([PositionType.LONG, PositionType.SHORT]),
+            self._prices[-1].actual_price, self._position_amount,
         )]  # First position
-        self._balance = self._initial_balance \
-            + self._positions[-1].amount * -self._position_opening_fee  # Opening fee
+        self._balance = self._initial_balance + self._positions[-1].amount * -self._position_opening_fee  # Opening fee
         observation = self._obtain_observation()
         info = {}
         return observation, info
 
     def step(self, action: np.int64) -> tuple[Dict[str, Any], SupportsFloat, bool, bool, dict[str, Any]]:
-        self._date += datetime.timedelta(days=1)
+        self._date_index += 1
         self._retrieve_prices()
         reward = self._positions[-1].amount * -self._position_holding_daily_fee  # Holding fee
         # If the position type changes, close the current position and open a new one
         if action != int(self._positions[-1].position_type):
             # Previous position's net
             reward += self._positions[-1].amount * self._last_position_net_ratio
-            self._positions.append(Position(self._prices[-1].time, self._position_amount, PositionType(action)))
+            self._positions.append(Position(
+                self._prices[-1].date, PositionType(action),
+                self._prices[-1].actual_price, self._position_amount,
+            ))
             reward += self._positions[-1].amount * -self._position_opening_fee  # Opening fee
         # Last position's net
         last_postion_net = self._positions[-1].amount * self._last_position_net_ratio
@@ -190,14 +205,10 @@ class TradingPlatform(gym.Env):
             # Liquidated
             self._balance + reward + last_postion_net < self._initial_balance * (1 - self._max_balance_loss)
             # Normally finished the episode without being forced to quit
-            or (
-                len(self._positions) >= self._min_positions_num
-                # Number of steps equals the number of dates between the date of first position and the current date.
-                and (self._date - self._positions[0].time.date()).days >= self._min_steps_num
-            )
+            or (len(self._positions) >= self._min_positions_num and self._date_index > self._min_steps_num)
         )
         # Truncation condition
-        truncated = self._date >= self._last_training_date
+        truncated = self._date_index >= len(self._date_range) - 1
         if terminated or truncated:
             reward += last_postion_net
         # Treat the balance as a cumulative reward in each episode
@@ -212,17 +223,19 @@ class TradingPlatform(gym.Env):
         figure = plt.figure()
         axes = figure.add_subplot(111)
         prices = self._asset.retrieve_historical_prices(
-            self._prices[-1].time,  # Use the exact datetime for the last price
-            (self._date - self._initial_date).days + self._historical_days_num,
+            self._date_range[self._date_index],
+            # Because we don't need to calculate the price delta when rendering,
+            # it is okay to retrieve the day before `self._historical_days_num`, hence `+ 1`.
+            self._date_index + self._historical_days_num + 1,
         )
         axes.plot(
-            [p.time for p in prices],
+            [p.date for p in prices],
             [p.actual_price for p in prices],
         )
         for position in self._positions:
             is_long = position.position_type == PositionType.LONG
             axes.plot(
-                position.time, self._retrieve_price(position.time),
+                position.date, position.entry_price,
                 marker=markers.CARETUP if is_long else markers.CARETDOWN,
                 color="green" if is_long else "red",
             )
@@ -231,42 +244,6 @@ class TradingPlatform(gym.Env):
             .reshape(figure.canvas.get_width_height()[::-1] + (3,))
         plt.close(figure)
         return image
-
-    def calc_earning(self, positions: List[Position], final_time: datetime.datetime) -> Tuple[float, float]:
-        if len(positions) < 1 or positions[-1].time > final_time:
-            return (0, 0)
-        earning = 0
-        # Nets and fees of closed positions (excluding the last position, as it is probably not yet closed)
-        for prev_position, cur_position in zip(positions[:-1], positions[1:]):
-            prev_price = self._retrieve_price(prev_position.time)
-            cur_price = self._retrieve_price(cur_position.time)
-            # Opening fee
-            earning += prev_position.amount * -self._position_opening_fee
-            # Holding fee
-            earning += (cur_position.time.date() - prev_position.time.date()).days \
-                * prev_position.amount * -self._position_holding_daily_fee
-            # Position net
-            earning += (cur_price / prev_price - 1) * (1 if prev_position.position_type == PositionType.LONG else -1) \
-                * prev_position.amount
-            logging.debug("%s %f %s", prev_position.time, prev_price, prev_position.position_type)
-        # Net and fee of the last position
-        earning += positions[-1].amount * -self._position_opening_fee
-        earning += (final_time.date() - positions[-1].time.date()).days \
-            * self._positions[-1].amount * -self._position_holding_daily_fee
-        earning += positions[-1].amount * self._last_position_net_ratio
-        logging.debug(
-            "%s %f %s",
-            positions[-1].time, self._retrieve_price(positions[-1].time), positions[-1].position_type,
-        )
-        # Actual price change
-        price_change = (self._retrieve_price(final_time) / self._retrieve_price(positions[0].time) - 1) \
-            * 1 * positions[0].amount  # Pure price change equals a LONG position, hence `1` instead of `-1`
-        return earning, price_change
-
-    @property
-    def _last_position_net_ratio(self) -> float:
-        return (self._prices[-1].actual_price / self._retrieve_price(self._positions[-1].time) - 1) \
-            * (1 if self._positions[-1].position_type == PositionType.LONG else -1)
 
     @property
     def _asset(self) -> DailyAsset:
@@ -282,17 +259,19 @@ class TradingPlatform(gym.Env):
         # The balance is mainly used for training, while using price as a position amount often implies testing.
         return 0 if self._use_price_as_position_amount else self._INITIAL_BALANCE_UNIT
 
-    def _retrieve_price(self, time: datetime.datetime) -> float:
-        return self._asset.retrieve_historical_prices(time, 1)[-1].actual_price
+    @property
+    def _last_position_net_ratio(self):
+        return calc_position_net_ratio(self._positions[-1], self._prices[-1].actual_price)
 
-    # Should be called right after updating `self._date` to the newest date
+    # Should be called right after updating `self._date_index` to the newest date
     def _retrieve_prices(self):
         # Since `retrieve_historical_prices` chooses the end price from a random time on the same end date,
         # multiple calls produce different results for the end price.
         # We need to avoid calling it again if the current date matches when we previously retrieved prices.
-        if len(self._prices) > 0 and self._prices[-1].time.date() == self._date:
+        date = self._date_range[self._date_index]
+        if len(self._prices) > 0 and self._prices[-1].date == date:
             return
-        self._prices = self._asset.retrieve_historical_prices(self._date, self._historical_days_num)
+        self._prices = self._asset.retrieve_historical_prices(date, self._historical_days_num)
 
     def _obtain_observation(self) -> Dict[str, Any]:
         # See: https://stackoverflow.com/questions/73922332/dict-observation-space-for-stable-baselines3-not-working
@@ -301,3 +280,33 @@ class TradingPlatform(gym.Env):
             "position_type": np.array([self._positions[-1].position_type], dtype=int),
             "position_net_ratio": np.array([self._last_position_net_ratio]),
         }
+
+
+def calc_position_net_ratio(position: Position, actual_price: float) -> float:
+    return (actual_price / position.entry_price - 1) * (1 if position.position_type == PositionType.LONG else -1)
+
+
+def calc_earning(
+    positions: List[Position], final_price: DailyPrice,
+    position_opening_fee: float, position_holding_daily_fee: float,
+) -> Tuple[float, float]:
+    if len(positions) < 1 or positions[-1].date > final_price.date:
+        return (0, 0)
+    earning = 0
+    # Nets and fees of closed positions (excluding the last position, as it is probably not yet closed)
+    for prev_position, cur_position in zip(positions[:-1], positions[1:]):
+        # Opening fee
+        earning += prev_position.amount * -position_opening_fee
+        # Holding fee
+        earning += (cur_position.date - prev_position.date).days * prev_position.amount * -position_holding_daily_fee
+        # Position net
+        earning += prev_position.amount * calc_position_net_ratio(prev_position, cur_position.entry_price)
+        logging.debug("%s %f %s", prev_position.date, prev_position.entry_price, prev_position.position_type)
+    # Net and fee of the last position
+    earning += positions[-1].amount * -position_opening_fee
+    earning += (final_price.date - positions[-1].date).days * positions[-1].amount * -position_holding_daily_fee
+    earning += positions[-1].amount * calc_position_net_ratio(positions[-1], final_price.actual_price)
+    logging.debug("%s %f %s", positions[-1].date, positions[-1].entry_price, positions[-1].position_type)
+    # Actual price change equals a LONG position, hence `1` instead of `-1`
+    actual_price_change = (final_price.actual_price / positions[0].entry_price - 1) * 1 * positions[0].amount
+    return earning, actual_price_change
