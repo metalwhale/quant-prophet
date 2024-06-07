0b99c2c08d1024f162faf53353f597d52bf569f8
Metal Whale <metalwhale.dev@gmail.com>
2024-06-07 21:47:43 +0900

    Set max days for stock.

--- trainer/trainer/asset/stock.py
+++ trainer/trainer/asset/stock.py
@@ -1,7 +1,7 @@
 import csv
 import datetime
 import os
-from typing import List
+from typing import List, Optional
 
 from .base import DailyAsset, DailyCandle
 
@@ -9,10 +9,12 @@ from .base import DailyAsset, DailyCandle
 class Stock(DailyAsset):
     _data_dir_path: os.PathLike
     _symbol: str
+    _max_days_num: int
 
-    def __init__(self, stock_dir_path: os.PathLike, symbol: str) -> None:
+    def __init__(self, stock_dir_path: os.PathLike, symbol: str, max_days_num: Optional[int] = None) -> None:
         self._data_dir_path = stock_dir_path
         self._symbol = symbol
+        self._max_days_num = max_days_num
         self._initialize()  # For fetching candles
 
     def _fetch_candles(self) -> List[DailyCandle]:
@@ -24,6 +26,8 @@ class Stock(DailyAsset):
                     datetime.datetime.strptime(row["date"].split(" ")[0], "%Y-%m-%d").date(),
                     float(row["high"]), float(row["low"]), float(row["close"]),
                 ))
+        if self._max_days_num is not None and self._max_days_num < len(candles):
+            candles = candles[-self._max_days_num:]
         return candles
 
     def _fetch_spot_price(self) -> float:
