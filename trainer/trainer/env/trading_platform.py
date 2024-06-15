e814587e7ef0099f76e577106fa701fa79c6d175
Metal Whale <metalwhale.dev@gmail.com>
2024-06-15 04:07:52 +0000

    Count chosen date. Don't choose date range based on training mode.

--- trainer/trainer/env/trading_platform.py
+++ trainer/trainer/env/trading_platform.py
@@ -1,7 +1,8 @@
 import datetime
 import logging
+from collections import defaultdict
 from enum import IntEnum
-from typing import Any, Dict, List, SupportsFloat, Tuple
+from typing import Any, Dict, List, Optional, SupportsFloat, Tuple
 
 import gymnasium as gym
 import numpy as np
@@ -55,7 +56,8 @@ class Position:
 class TradingPlatform(gym.Env):
     metadata = {"render_modes": ["rgb_array"]}
 
-    _is_training_mode: bool
+    # Control flags
+    is_training_mode: bool
 
     # Terminology
     # "Episode" and "step":
@@ -82,7 +84,7 @@ class TradingPlatform(gym.Env):
     #   |       |                                                                 |                                             |
     #   ~~~~~~~~|=================================================================|=============================================| Now
     #   |       |                                                                 |
-    #   |                                                                         Last training date (`_last_training_date`)
+    #   |                                                                         Last training date
     #   |
     #   |<----->| The first `historical_days_num` days are non-tradable and will be skipped
     #
@@ -99,7 +101,6 @@ class TradingPlatform(gym.Env):
     # Hyperparameters
     _asset_pool: AssetPool
     _historical_days_num: int  # Number of days used for retrieving historical data
-    _last_training_date: datetime.date
     # TODO: Reconsider the meaning of the opening fee.
     # I believe that changing the opening fee affects how often new positions are opened,
     # i.e., increasing the opening fee means the model may learn to open fewer positions.
@@ -114,11 +115,15 @@ class TradingPlatform(gym.Env):
     _min_steps_num: int  # Minimum number of steps allowed in one episode
 
     # State components
-    _asset_symbol: str  # Changed only if we reset
+    # Platform-level, only changed if we refresh
+    _polarity_diff: int
+    _date_chosen_counter: Dict[datetime.date, Dict[str, int]]  # Metadata
+    # Episode-level, only changed if we reset to begin a new episode
+    _asset_symbol: str  # Symbol of the current asset
     _date_range: List[datetime.date]  # The random date range of each episode
+    # Step-level, likely changed at each step
     _date_index: int  # Grows in the same episode, resets to 0 for a new episode
     _prices: List[DailyPrice]  # Updated whenever the date changes
-    _polarity_diff: int
     _positions: List[Position]  # Keeps adding positions in the same episode, clears them all for a new episode
     _balance: float  # Resets to initial balance after each episode
 
@@ -132,7 +137,6 @@ class TradingPlatform(gym.Env):
         self,
         asset_pool: AssetPool,
         historical_days_num: int,
-        last_training_date: datetime.date,
         position_opening_fee: float = 0.0,
         max_balance_loss: float = 0.0,
         min_positions_num: int = 0,
@@ -142,16 +146,12 @@ class TradingPlatform(gym.Env):
         # Hyperparameters
         self._asset_pool = asset_pool
         self._historical_days_num = historical_days_num
-        self._last_training_date = last_training_date  # TODO: Check if there are enough days to retrieve historical data
         # These following hyperparameters are mainly used only for training,
         # by calculating reward and determining whether to terminate an episode.
         self._position_opening_fee = position_opening_fee
         self._max_balance_loss = max_balance_loss
         self._min_positions_num = min_positions_num
         self._min_steps_num = min_steps_num
-        # State components
-        self._prices = []
-        self._polarity_diff = 0
         # Environment
         self.action_space = gym.spaces.Discrete(len(PositionType))
         # NOTE: Theoretically, we only need the historical price when deciding the position order (buy/sell or hold).
@@ -169,8 +169,8 @@ class TradingPlatform(gym.Env):
             # TODO: Consider cases when position net ratio can be greater than 1.
             "position_net_ratio": gym.spaces.Box(-1, 1, shape=(1,)),
         })
-        # Mode
-        self.is_training_mode = True
+        # Refresh platform-level state components
+        self.refresh()
 
     def reset(
         self, *,
@@ -189,6 +189,9 @@ class TradingPlatform(gym.Env):
         self._balance = self._initial_balance + self._positions[-1].amount * -self._position_opening_fee  # Opening fee
         observation = self._obtain_observation()
         info = {}
+        if self.is_training_mode:
+            self._date_chosen_counter[self._date_range[self._date_index]][self._asset_symbol] += 1
+            self._polarity_diff += calc_polarity_diff(self._prices[-1].price_delta)
         return observation, info
 
     def step(self, action: np.int64) -> tuple[Dict[str, Any], SupportsFloat, bool, bool, dict[str, Any]]:
@@ -224,6 +227,8 @@ class TradingPlatform(gym.Env):
         self._balance += reward
         observation = self._obtain_observation()
         info = {}
+        if self.is_training_mode:
+            self._polarity_diff += calc_polarity_diff(self._prices[-1].price_delta)
         return observation, reward, terminated, truncated, info
 
     def render(self) -> Any | List[Any] | None:
@@ -231,17 +236,16 @@ class TradingPlatform(gym.Env):
             return
         figure = plt.figure(figsize=(10, 6), dpi=200)
         figure.subplots_adjust(left=0.05, bottom=0.05, right=1, top=1)
-        axes = figure.add_subplot(111)
+        # Plot prices and positions
+        axes = figure.add_subplot(211)
         prices = self._asset.retrieve_historical_prices(
             self._date_range[self._date_index],
             # Retrieve all the days before the current date in the date range
             self._date_index + self._historical_days_num,
             indeterministic=self.is_training_mode,
         )
-        axes.plot(
-            [p.date for p in prices],
-            [p.actual_price for p in prices],
-        )
+        dates = [p.date for p in prices]
+        axes.plot(dates, [p.actual_price for p in prices])
         for position in self._positions:
             is_long = position.position_type == PositionType.LONG
             axes.plot(
@@ -249,25 +253,25 @@ class TradingPlatform(gym.Env):
                 marker=markers.CARETUP if is_long else markers.CARETDOWN,
                 color="green" if is_long else "red",
             )
+        # Plot date counter
+        axes = figure.add_subplot(212)
+        axes.bar(dates, [sum(self._date_chosen_counter[d].values()) for d in dates])
+        # Draw figure
         figure.canvas.draw()
         image = np.frombuffer(figure.canvas.tostring_rgb(), dtype=np.uint8) \
             .reshape(figure.canvas.get_width_height()[::-1] + (3,))
         plt.close(figure)
         return image
 
-    @property
-    def is_training_mode(self) -> bool:
-        return self._is_training_mode
-
-    @is_training_mode.setter
-    def is_training_mode(self, is_training_mode: bool):
-        self._is_training_mode = is_training_mode
+    def apply_date_range(self, min_date: Optional[datetime.date] = None, max_date: Optional[datetime.date] = None):
         self._asset_pool.apply_date_range_matcher(
-            self._min_steps_num,
-            self._historical_days_num,
-            min_date=self._last_training_date if not self.is_training_mode else None,
-            max_date=self._last_training_date if self.is_training_mode else None,
+            self._min_steps_num, self._historical_days_num,
+            min_date=min_date, max_date=max_date,
         )
+
+    def refresh(self):
+        self._polarity_diff = 0
+        self._date_chosen_counter = defaultdict(lambda: defaultdict(int))
 
     @property
     def _asset(self) -> DailyAsset:
@@ -293,13 +297,12 @@ class TradingPlatform(gym.Env):
         # multiple calls produce different results for the end price.
         # We need to avoid calling it again if the current date matches when we previously retrieved prices.
         date = self._date_range[self._date_index]
-        if len(self._prices) > 0 and self._prices[-1].date == date:
+        if hasattr(self, "_prices") and len(self._prices) > 0 and self._prices[-1].date == date:
             return
         self._prices = self._asset.retrieve_historical_prices(
             date, self._historical_days_num,
             indeterministic=self.is_training_mode,
         )
-        self._polarity_diff += calc_polarity_diff(self._prices[-1].price_delta)
 
     def _obtain_observation(self) -> Dict[str, Any]:
         # See: https://stackoverflow.com/questions/73922332/dict-observation-space-for-stable-baselines3-not-working
