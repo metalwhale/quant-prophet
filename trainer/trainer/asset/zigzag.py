c23e2537b20c499e86fc590a682b014fac32dec2
Metal Whale <metalwhale.dev@gmail.com>
2024-07-01 22:51:10 +0900

    Zigzag asset uses only up and down trend.

--- trainer/trainer/asset/zigzag.py
+++ trainer/trainer/asset/zigzag.py
@@ -8,7 +8,6 @@ from .base import DailyAsset, DailyCandle
 
 
 class TrendType(Enum):
-    STAGNANT = 0
     UP = 1
     DOWN = 2
 
@@ -16,11 +15,9 @@ class TrendType(Enum):
 class Zigzag(DailyAsset):
     _published_date: datetime.date
     _published_price: float
-    _trend_type_weights: Tuple[float, float, float]  # (STAGNANT, UP, DOWN)
-    _stagnant_period_range: Tuple[int, int]
-    _stagnant_movement_range: Tuple[float, float]  # Ratio, exclusive end
-    _active_period_range: Tuple[int, int]
-    _active_movement_magnitude_range: Tuple[float, float]  # Positive ratio, exclusive end
+    _trend_type_weights: Tuple[float, float]  # (UP, DOWN)
+    _trend_period_range: Tuple[int, int]
+    _trend_movement_magnitude_range: Tuple[float, float]  # Positive ratio, exclusive end
     _fluctuation_range: Tuple[float, float]  # Ratio, exclusive end
 
     _MA_WINDOW_RANGE: Tuple[int, int] = (5, 20)  # TODO: Choose a better window length
@@ -30,29 +27,23 @@ class Zigzag(DailyAsset):
         symbol: str,
         published_date: datetime.date,
         published_price: float,
-        trend_type_weights: Tuple[float, float, float],
-        stagnant_period_range: Tuple[int, int],
-        stagnant_movement_range: Tuple[float, float],
-        active_period_range: Tuple[int, int],
-        active_movement_magnitude_range: Tuple[float, float],
+        trend_type_weights: Tuple[float, float],
+        trend_period_range: Tuple[int, int],
+        trend_movement_magnitude_range: Tuple[float, float],
         fluctuation_range: Tuple[float, float],
     ) -> None:
         super().__init__(symbol)
         if (
-            (stagnant_period_range[0] > stagnant_period_range[1] or stagnant_period_range[0] < 0)
-            or (stagnant_movement_range[0] > 0 or stagnant_movement_range[1] < 0)
-            or (active_period_range[0] > active_period_range[1] or active_period_range[0] < 0)
-            or (active_movement_magnitude_range[0] < 0 or active_movement_magnitude_range[1] < 0)
+            (trend_period_range[0] > trend_period_range[1] or trend_period_range[0] < 0)
+            or (trend_movement_magnitude_range[0] < 0 or trend_movement_magnitude_range[1] < 0)
             or (fluctuation_range[0] > 0 or fluctuation_range[1] < 0)
         ):
             raise ValueError
         self._published_date = published_date
         self._published_price = published_price
         self._trend_type_weights = trend_type_weights
-        self._stagnant_period_range = stagnant_period_range
-        self._stagnant_movement_range = stagnant_movement_range
-        self._active_period_range = active_period_range
-        self._active_movement_magnitude_range = active_movement_magnitude_range
+        self._trend_period_range = trend_period_range
+        self._trend_movement_magnitude_range = trend_movement_magnitude_range
         self._fluctuation_range = fluctuation_range
         self._initialize()  # For fetching candles
 
@@ -69,27 +60,16 @@ class Zigzag(DailyAsset):
             price = 0.0
             if date == trend_end_date:
                 # Trend type for next trend
-                trend_type = np.random.choice(
-                    [TrendType.STAGNANT, TrendType.UP, TrendType.DOWN],
-                    p=self._trend_type_weights,
-                )
+                trend_type = np.random.choice([TrendType.UP, TrendType.DOWN], p=self._trend_type_weights)
                 # Number of days for next trend
-                trend_days = 0
-                if trend_type == TrendType.STAGNANT:
-                    trend_days = np.random.randint(*self._stagnant_period_range)
-                else:
-                    trend_days = np.random.randint(*self._active_period_range)
+                trend_days = np.random.randint(*self._trend_period_range)
                 # Movement for next trend
-                movement_ratio = 1.0
-                if trend_type == TrendType.STAGNANT:
-                    movement_ratio += np.random.uniform(*self._stagnant_movement_range)
-                else:
-                    movement_magnitude_ratio = trend_days * np.random.uniform(*self._active_movement_magnitude_range)
-                    movement_ratio += (1 if trend_type == TrendType.UP else -1) * movement_magnitude_ratio
+                movement_magnitude_ratio = trend_days * np.random.uniform(*self._trend_movement_magnitude_range)
+                movement_ratio = 1.0 + (1 if trend_type == TrendType.UP else -1) * movement_magnitude_ratio
                 # Move to next trend
                 trend_start_date = trend_end_date
                 trend_start_price = trend_end_price
-                if (last_date - date).days < self._active_period_range[1]:
+                if (last_date - date).days < self._trend_period_range[1]:
                     trend_end_date = last_date
                 else:
                     trend_end_date += datetime.timedelta(days=trend_days)
