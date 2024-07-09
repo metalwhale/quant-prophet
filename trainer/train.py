2f33304cef42595378ab6454c93a0f9b6c5c7697
Tran Duc Son <tran.duc.son@moneyforward.co.jp>
2024-07-09 09:23:26 +0900

    Use multiple date ranges for evaluation.

--- trainer/train.py
+++ trainer/train.py
@@ -14,23 +14,18 @@ from trainer.env.trading_platform import TradingPlatform
 from trainer.env.evaluation import FullEvalCallback
 
 
-MONTHLY_TRADABLE_DAYS_NUM = 20
-YEARLY_TRADABLE_DAYS_NUM = 250
-LAST_TRAINING_DATE = datetime.datetime.strptime("2019-12-31", "%Y-%m-%d").date()
-LAST_VALIDATION_DATE = datetime.datetime.strptime("2022-12-31", "%Y-%m-%d").date()
-HISTORICAL_DAYS_NUM = MONTHLY_TRADABLE_DAYS_NUM * 6
-
-# LINK: Ignore the last position type (SIDELINE), use only BUY and SELL
-INTEREST_RATE = 0.0  # Each year
-POSITION_HOLDING_DAILY_FEE = INTEREST_RATE / YEARLY_TRADABLE_DAYS_NUM
-POSITION_OPENING_PENALTY = 0.01
-
-STOCK_MAX_DAYS_NUM = None  # YEARLY_TRADABLE_DAYS_NUM * 20
-STOCK_SYMBOLS = ["AAPL"]
-ZIGZAG_PUBLISHED_DATE = datetime.datetime.strptime("2000-01-01", "%Y-%m-%d").date()
-
-
 def generate_envs(assets: List[DailyAsset]) -> Tuple[TradingPlatform, Dict[str, TradingPlatform]]:
+    MONTHLY_TRADABLE_DAYS_NUM = 20
+    YEARLY_TRADABLE_DAYS_NUM = 250
+    LAST_TRAINING_DATE = datetime.datetime.strptime("1999-12-31", "%Y-%m-%d").date()
+    EVAL_DATE_RANGES = [
+        datetime.datetime.strptime(d, "%Y-%m-%d").date() if d is not None else None
+        for d in ["2004-12-31", "2009-12-31", "2014-12-31", "2019-12-31", None]
+    ]
+    HISTORICAL_DAYS_NUM = MONTHLY_TRADABLE_DAYS_NUM * 6
+    YEARLY_INTEREST_RATE = 0.0  # LINK: Ignore the last position type (SIDELINE), use only BUY and SELL
+    POSITION_HOLDING_DAILY_FEE = YEARLY_INTEREST_RATE / YEARLY_TRADABLE_DAYS_NUM
+    POSITION_OPENING_PENALTY = 0.01
     # Training environment
     train_asset_pool = AssetPool(
         assets,
@@ -54,48 +49,43 @@ def generate_envs(assets: List[DailyAsset]) -> Tuple[TradingPlatform, Dict[str, 
         position_holding_daily_fee=POSITION_HOLDING_DAILY_FEE, position_opening_penalty=POSITION_OPENING_PENALTY,
     )
     rep_train_env.is_training = False
-    # Validation environment
-    val_asset_pool = AssetPool(assets)
-    val_asset_pool.apply_date_range(
-        (LAST_TRAINING_DATE, LAST_VALIDATION_DATE), HISTORICAL_DAYS_NUM,
-        excluding_historical=False,
-    )
-    val_env = TradingPlatform(
-        val_asset_pool, HISTORICAL_DAYS_NUM,
-        position_holding_daily_fee=POSITION_HOLDING_DAILY_FEE, position_opening_penalty=POSITION_OPENING_PENALTY,
-    )
-    val_env.is_training = False
-    # Test environment
-    test_asset_pool = AssetPool(assets)
-    test_asset_pool.apply_date_range(
-        (LAST_VALIDATION_DATE, None), HISTORICAL_DAYS_NUM,
-        excluding_historical=False,
-    )
-    test_env = TradingPlatform(
-        test_asset_pool, HISTORICAL_DAYS_NUM,
-        position_holding_daily_fee=POSITION_HOLDING_DAILY_FEE, position_opening_penalty=POSITION_OPENING_PENALTY,
-    )
-    test_env.is_training = False
-    return train_env, {"train": rep_train_env, "val": val_env, "test": test_env}
+    # Evaluation environments
+    eval_envs: Dict[str, TradingPlatform] = {"train": rep_train_env}  # Use train env for evaluation as well
+    for i, last_eval_date in enumerate(EVAL_DATE_RANGES):
+        eval_asset_pool = AssetPool(assets)
+        eval_asset_pool.apply_date_range(
+            (LAST_TRAINING_DATE if i == 0 else EVAL_DATE_RANGES[i - 1], last_eval_date), HISTORICAL_DAYS_NUM,
+            excluding_historical=False,
+        )
+        eval_env = TradingPlatform(
+            eval_asset_pool, HISTORICAL_DAYS_NUM,
+            position_holding_daily_fee=POSITION_HOLDING_DAILY_FEE, position_opening_penalty=POSITION_OPENING_PENALTY,
+        )
+        eval_env.is_training = False
+        eval_envs[f"eval{i}"] = eval_env
+    return train_env, eval_envs
 
 
-def generate_stock_assets(stock_symbols: List[str]) -> List[Stock]:
+def generate_stock_assets() -> List[Stock]:
+    MAX_DAYS_NUM = None  # YEARLY_TRADABLE_DAYS_NUM * 20
+    SYMBOLS: List[str] = ["AAPL"]
     assets = [
         Stock(
             symbol, Path(__file__).parent.parent / "data" / "stock" / "input" / "us",
-            max_days_num=STOCK_MAX_DAYS_NUM,
+            max_days_num=MAX_DAYS_NUM,
         )
-        for symbol in stock_symbols
+        for symbol in SYMBOLS
     ]
     return assets
 
 
 def generate_zigzag_assets(assets_num: int) -> List[Zigzag]:
+    PUBLISHED_DATE = datetime.datetime.strptime("1980-01-01", "%Y-%m-%d").date()  # Before `LAST_TRAINING_DATE`
     assets = [
         Zigzag(
             datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "_"
             + "".join(np.random.choice([*(string.ascii_letters + string.digits)], size=4)),
-            ZIGZAG_PUBLISHED_DATE, np.random.uniform(0, 10),
+            PUBLISHED_DATE, np.random.uniform(0, 10),
             (0.55, 0.45), (2, 6), (0.0025, 0.005), (-0.02, 0.02),
         )
         for _ in range(assets_num)
@@ -107,7 +97,7 @@ def train(env_type: str):
     train_env: TradingPlatform
     eval_envs: Dict[str, TradingPlatform]
     if env_type == "stock":
-        train_env, eval_envs = generate_envs(generate_stock_assets(STOCK_SYMBOLS))
+        train_env, eval_envs = generate_envs(generate_stock_assets())
     elif env_type == "zigzag":
         train_env, eval_envs = generate_envs(generate_zigzag_assets(5))
     now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
