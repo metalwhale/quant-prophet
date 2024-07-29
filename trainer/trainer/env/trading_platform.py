405cb26de08191391c71ae31d4e8afa371272b51
Metal Whale <metalwhale.dev@gmail.com>
2024-07-30 01:59:09 +0900

    Try to reduce noise by smoothing position net.

--- trainer/trainer/env/trading_platform.py
+++ trainer/trainer/env/trading_platform.py
@@ -265,14 +265,14 @@ class TradingPlatform(gym.Env):
             # LINK: `num` and `clear` help prevent memory leak (See: https://stackoverflow.com/a/65910539)
             num=f"trading_platform.{self.figure_num}",
             # NOTE: Remember to increase the height of `figsize` if you add more plots
-            figsize=(len(dates) / YEARLY_TRADABLE_DAYS_NUM * YEAR_WIDTH, 6 * SUBPLOT_HEIGHT),
+            figsize=(len(dates) / YEARLY_TRADABLE_DAYS_NUM * YEAR_WIDTH, 7 * SUBPLOT_HEIGHT),
             dpi=400,
             clear=True,
         )
         figure.subplots_adjust(left=100 / len(dates), bottom=0.02, right=0.99, top=0.95)
         all_axes: List[Tuple[str, Axes, Dict[datetime.date, float]]] = []
         # Plot prices
-        axes = figure.add_subplot(611)
+        axes = figure.add_subplot(711)
         all_axes.append(("Prices", axes, {p.date: p.actual_price for p in prices}))
         position_index: Optional[int] = None
         date_prices: List[Tuple[datetime.date, float]] = []
@@ -307,26 +307,30 @@ class TradingPlatform(gym.Env):
                 # Move to next position
                 position_index = next_position_index
                 date_prices = [date_price]
+        # Plot smoothed prices
+        axes = figure.add_subplot(712)
+        all_axes.append(("Smoothed prices", axes, {p.date: p.smoothed_price for p in prices}))
+        axes.plot(dates, [p.smoothed_price if p.date in dates else None for p in prices])
         # Plot EMA diff ratios
-        axes = figure.add_subplot(612)
+        axes = figure.add_subplot(713)
         all_axes.append(("EMA diff ratios", axes, {p.date: p.ema_diff_ratio for p in prices}))
         axes.plot(dates, [0 for _ in dates], color="gray")
         axes.plot(dates, [p.ema_diff_ratio if p.date in dates else None for p in prices], color="orange")
         # Plot scaled RSIs
-        axes = figure.add_subplot(613)
+        axes = figure.add_subplot(714)
         all_axes.append(("RSIs", axes, {p.date: p.scaled_rsi for p in prices}))
         axes.plot(dates, [p.scaled_rsi if p.date in dates else None for p in prices])
         # Plot scaled ADXs
-        axes = figure.add_subplot(614)
+        axes = figure.add_subplot(715)
         all_axes.append(("ADXs", axes, {p.date: p.scaled_adx for p in prices}))
         axes.plot(dates, [p.scaled_adx if p.date in dates else None for p in prices])
         # Plot scaled CCIs
-        axes = figure.add_subplot(615)
+        axes = figure.add_subplot(716)
         all_axes.append(("CCIs", axes, {p.date: p.scaled_cci for p in prices}))
         axes.plot(dates, [p.scaled_cci if p.date in dates else None for p in prices])
         # Plot action values
         action_values = self._extra_info.action_values
-        axes = figure.add_subplot(616)
+        axes = figure.add_subplot(717)
         all_axes.append(("Action values", axes, {d: v[2] for d, v in action_values.items()}))
         axes.plot(dates, [0 for _ in dates], color="gray")
         axes.plot(dates, [action_values[d][0] if d in action_values else None for d in dates], color="green", alpha=0.2)
