f5ef1d488af07c7332035baf45af1283efa660d8
Metal Whale <metalwhale.dev@gmail.com>
2024-07-30 00:58:03 +0900

    Add fluctuation to sinusoid.

--- trainer/trainer/asset/sinusoid.py
+++ trainer/trainer/asset/sinusoid.py
@@ -11,7 +11,7 @@ OPENING_HOUR: int = 8
 CLOSING_HOUR: int = 16
 
 
-class Sinusoid(DailyAsset):
+class ComposedSinusoid(DailyAsset):
     _published_time: datetime.datetime
     _get_price: Callable[[datetime.datetime], float]
 
@@ -19,11 +19,21 @@ class Sinusoid(DailyAsset):
         self,
         symbol: str,
         published_time: datetime.datetime,
-        alpha: float = math.pi, beta: float = 1.0, gamma1: float = 0.0, gamma2: float = 0.0,
+        components_num: int,
+        alpha_range: Tuple[float, float], beta_range: Tuple[float, float],
+        gamma1_range: Tuple[float, float], gamma2_range: Tuple[float, float],
+        fluctuation_scale_range: Tuple[float, float],
     ) -> None:
-        super().__init__(symbol)
+        DailyAsset.__init__(self, symbol)
         self._published_time = published_time
-        self._get_price = lambda time: sine(time, self._published_time, alpha, beta, gamma1, gamma2)
+        components = []
+        for _ in range(components_num):
+            components.append(self._generate_sine(
+                alpha_range, beta_range,
+                gamma1_range, gamma2_range,
+                fluctuation_scale_range,
+            ))
+        self._get_price = lambda time: sum([c(time) for c in components])
         self._initialize()  # For fetching candles
 
     def _fetch_candles(self) -> List[DailyCandle]:
@@ -40,34 +50,18 @@ class Sinusoid(DailyAsset):
             date += datetime.timedelta(days=1)
         return candles
 
-
-class ComposedSinusoid(Sinusoid):
-    def __init__(
-        self,
-        symbol: str,
-        published_time: datetime.datetime,
-        components_num: int,
-        alpha_range: Tuple[float, float], beta_range: Tuple[float, float],
-        gamma1_range: Tuple[float, float], gamma2_range: Tuple[float, float],
-    ) -> None:
-        DailyAsset.__init__(self, symbol)
-        self._published_time = published_time
-        components = []
-        for _ in range(components_num):
-            components.append(self._generate_sine(alpha_range, beta_range, gamma1_range, gamma2_range))
-        self._get_price = lambda time: sum([c(time) for c in components])
-        self._initialize()  # For fetching candles
-
     def _generate_sine(
         self,
         alpha_range: Tuple[float, float], beta_range: Tuple[float, float],
         gamma1_range: Tuple[float, float], gamma2_range: Tuple[float, float],
+        fluctuation_scale_range: Tuple[float, float],
     ) -> Callable[[datetime.datetime], float]:
         alpha = np.random.uniform(*alpha_range)
         beta = np.random.uniform(*beta_range)
         gamma1 = np.random.uniform(*gamma1_range)
         gamma2 = np.random.uniform(*gamma2_range)
-        return lambda time: sine(time, self._published_time, alpha, beta, gamma1, gamma2)
+        fluctuation_scale = np.random.uniform(*fluctuation_scale_range)
+        return lambda time: sine(time, self._published_time, alpha, beta, gamma1, gamma2, fluctuation_scale)
 
 
 @staticmethod
@@ -75,7 +69,10 @@ class ComposedSinusoid(Sinusoid):
 def sine(
     time: datetime.datetime, published_time: datetime.datetime,
     alpha: float, beta: float, gamma1: float, gamma2: float,
+    fluctuation_scale: float,
 ) -> float:
     # Use 1 day (86400 seconds) as 1 unit of time.
     x = (time - published_time).total_seconds() / 86400
-    return math.sin(alpha * x + gamma1) * beta + gamma2
+    y = math.sin(alpha * x + gamma1) * beta + gamma2
+    y *= 1 + np.random.laplace(0, fluctuation_scale)
+    return y
